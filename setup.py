f401bdafe71c740dd31e34d6f948b64dae232d92
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2015-11-25 13:08:19 -0500

    Added "import sys" for sys.version_info.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import sys
 
 install_requires = ['eupy >=1.0, <2.0']
 dependency_links = ['git+https://github.com/jedevc/EuPy.git@7b48c35e96a1775ee37c4e5da8d3de46e99e609c#egg=eupy-1.0']
