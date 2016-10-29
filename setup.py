5ac61a22f1b575fa549619386d1c7db3ac110770
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-10-29 17:37:51 -0400

    Bump version number
    0.2.8 includes support for !inline and (self)/(@self) variables.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='botbot',
-    version='0.2.7',
+    version='0.2.8',
     description='A meta-bot for Euphoria.',
     author='Rishov Sarkar',
     url='https://github.com/ArkaneMoose/BotBot',
