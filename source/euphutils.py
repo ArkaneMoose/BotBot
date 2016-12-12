24abaf62679a126229346f623529907aeec87104
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-12-12 17:28:14 -0500

    Fix #43
    This commit fixes #43 by essentially disabling comments on commands like
    `!help`, thus preventing the bot from responding to messages like `!help
    something @AnotherBot`.

--- source/euphutils.py
+++ source/euphutils.py
@@ -13,7 +13,7 @@ class EuphUtils:
     @classmethod
     def command(cls, name, nickname=None, match_string=None):
         if nickname == '':
-            return re.compile(name + r'\b\s*(?!@\S+)(\S.*|$)', re.IGNORECASE + re.DOTALL + re.MULTILINE)
+            return re.compile(name + r'\b\s*$', re.IGNORECASE + re.DOTALL + re.MULTILINE)
         elif not nickname:
             return re.compile(name + r'\b\s*(.*)', re.IGNORECASE + re.DOTALL + re.MULTILINE)
         return re.compile(name + r'\s+' + re.escape(cls.mention(nickname)) + r'(?:\b|$|(?=\s))\s*(.*)', re.IGNORECASE + re.DOTALL + re.MULTILINE)
