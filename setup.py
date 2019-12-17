003050b978dbc4754465c7b829957178cfd6f6b2
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2019-12-16 23:32:43 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='botbot',
-    version='0.5.2',
+    version='0.5.3',
     description='A meta-bot for Euphoria.',
     author='Rishov Sarkar',
     url='https://github.com/ArkaneMoose/BotBot',
