ee0c100f63b93993d4bb0216bd9f904bc47007a9
JRF <jeremyredhead@users.noreply.github.com>
2025-12-01 22:44:34 -0600

    Update EuPy version
    Replaces euphoria.io (RIP) with euphoria.leet.nu

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='MIT',
     packages=['botbot'],
     install_requires=['eupy >=1.2, <2.0', 'simpleeval >=0.9, <0.10'],
-    dependency_links=['git+https://github.com/ArkaneMoose/EuPy.git@75777c49503acb32e09f4c36f6f65cc35157694a#egg=eupy-1.2', 'git+https://github.com/ArkaneMoose/simpleeval.git@ac33b805645ca616f11e64bb3330a12bc5fba658#egg=simpleeval-0.9.2'],
+    dependency_links=['git+https://github.com/ArkaneMoose/EuPy.git@2716bc40d7514726d85f22720a3ec0fc98471a43#egg=eupy-1.2.1', 'git+https://github.com/ArkaneMoose/simpleeval.git@ac33b805645ca616f11e64bb3330a12bc5fba658#egg=simpleeval-0.9.2'],
     entry_points={
         'console_scripts': [
             'botbot = botbot.__main__:main'
